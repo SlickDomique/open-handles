21fa28b8fef46f40b2143cfe72d5ac626ac0baf0
domi <slickdomique@proton.me>
2024-09-01 20:26:04 +0200

    Temporary removal of lesbian.cat from the list

--- src/utils/domains.ts
+++ src/utils/domains.ts
@@ -6,6 +6,6 @@ export enum DomainType {
 export const domains: { [domain: string]: DomainType } = {
   "the-gay.cat": DomainType.FILE,
   "woke.cat": DomainType.FILE,
-  "lesbian.cat": DomainType.FILE,
+  // "lesbian.cat": DomainType.FILE,
   "is-extremely.gay": DomainType.FILE,
 };
